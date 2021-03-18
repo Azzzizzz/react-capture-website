81eeec112fdc4df523e78f1e1bf71835bdf9d4d7
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-03-18 23:35:57 +0530

    9. React Router

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,22 @@
 import styled from "styled-components";
-
+import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <StyledNav>
       <h1>
-        <a id="logo" href="#">
+        <Link id="logo" to="/">
           Capture
-        </a>
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href="#">About Us</a>
+          <Link to="/">About Us</Link>
         </li>
         <li>
-          <a href="#">Out Work</a>
+          <Link to="/work">Out Work</Link>
         </li>
         <li>
-          <a href="#">Contact Us</a>
+          <Link to="/contact">Contact Us</Link>
         </li>
       </ul>
     </StyledNav>
